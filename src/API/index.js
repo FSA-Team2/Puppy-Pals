efb72cf64671274ccb4f87c1d871ca010f6c3784
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-08-04 19:34:03 -0400

    added the API link and default function players to the index.js file located in the API folder

---
+++ src/API/index.js
@@ -0,0 +1,16 @@
+const API=`https://fsa-puppy-bowl.herokuapp.com/2302-acc-pt-web-pt-a/`;
+
+
+export default async function players() {
+
+    try {
+        const response= await fetch(API);
+        const result= await response.json();
+        return result();
+        
+    } catch (error) {
+        console.error("error fetching player data", error)
+        
+    }
+    
+}
