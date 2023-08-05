ba8dff2947af577d5158b24e6b8f94ce763e7ae2
Mohamed Faki <hammadi.faki@gmail.com>
2023-08-05 15:44:05 -0400

    not sure

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App></App>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
