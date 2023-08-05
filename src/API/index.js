ac8175d1705ed08534ba4c15dd46d7c156dbb260
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-08-04 22:46:32 -0400

    useState and useEffect fetch funtions in the AllPlayers componet file is not working.

--- src/API/index.js
+++ src/API/index.js
@@ -4,9 +4,10 @@ const API=`https://fsa-puppy-bowl.herokuapp.com/2302-acc-pt-web-pt-a/`;
 export default async function players() {
 
     try {
+
         const response= await fetch(API);
         const result= await response.json();
-        return result();
+        return result;
         
     } catch (error) {
         console.error("error fetching player data", error)
--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,17 +1,57 @@
 // import React from 'react';
-import SinglePlayer from './SinglePlayer';
+import SinglePlayer from "./SinglePlayer";
 import { Routes, Route } from "react-router-dom";
-
+import players from "../API";
+import { useState, useEffect } from 'react'
 
 const AllPlayers = () => {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = `https://fsa-puppy-bowl.herokuapp.com/2302-acc-pt-web-pt-a/`;
+
+    async function fetchP() {
+      try {
+        const response = await players(apiUrl);
+        // const data = await response.json();
+
+        setPlayers(response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchP();
+  }, []);
+
   return (
     <div>
-    <Routes>
-      <Route path='/' element={<AllPlayers />} />
-      <Route path='/players/:id' element={<SinglePlayer />} />
-    </Routes>
+    {
+      players.map((players) => {
+        return (
+          <div className="button" onClick={() => setPlayers(players.id)} key={players.is}><h4>{players.name}</h4>
+
+          
+          
+      <Routes>
+       <Route path="/" element={<AllPlayers />} />
+        <Route path="/players/:id" element={<SinglePlayer />} />
+      </Routes>
+    </div> 
+
+
+
+        )
+      })
+    }
     </div>
-  );
-};
+
+
+
+
+
+
+     
+   );
+ };
 
 export default AllPlayers;
